efc7132d35d8d2c627aa8745efa4125ae23a9f70
Tom E <guacaplushy@icloud.com>
2022-12-08 02:10:46 +0000

    Initial commit

---
+++ fix.sh
@@ -0,0 +1,522 @@
+mkdir -p logs
+set -e
+
+{
+
+echo "[*] Command ran:`if [ $EUID = 0 ]; then echo " sudo"; fi` ./palera1n.sh $@"
+
+# =========
+# Variables
+# =========
+ipsw="" # IF YOU WERE TOLD TO PUT A CUSTOM IPSW URL, PUT IT HERE. YOU CAN FIND THEM ON https://appledb.dev
+version="1.4.0"
+os=$(uname)
+dir="$(pwd)/binaries/$os"
+commit=$(git rev-parse --short HEAD)
+branch=$(git rev-parse --abbrev-ref HEAD)
+max_args=1
+arg_count=0
+disk=8
+fs=disk0s1s$disk
+#fs=disk1s7
+log="$(date +%T)"-"$(date +%F)"-"$(uname)"-"$(uname -r)"
+touch logs/${log}.log
+
+# =========
+# Functions
+# =========
+remote_cmd() {
+    "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "$@"
+}
+remote_cp() {
+    "$dir"/sshpass -p 'alpine' scp -o StrictHostKeyChecking=no -P2222 $@
+}
+
+step() {
+    for i in $(seq "$1" -1 1); do
+        printf '\r\e[1;36m%s (%d) ' "$2" "$i"
+        sleep 1
+    done
+    printf '\r\e[0m%s (0)\n' "$2"
+}
+
+print_help() {
+    cat << EOF
+Usage: $0 [Options] [ subcommand | iOS version ]
+iOS 15.0-16.1.1 jailbreak tool for checkm8 devices
+
+Options:
+    --help              Print this help
+    --semi-tethered     When used with --tweaks, make the jailbreak semi-tethered instead of tethered
+    --dfuhelper         A helper to help get A11 devices into DFU mode from recovery mode
+    --no-baseband       Indicate that the device does not have a baseband
+    --debug             Debug the script
+
+Subcommands:
+    dfuhelper           An alias for --dfuhelper
+
+The iOS version argument should be the iOS version of your device.
+It is required when starting from DFU mode.
+EOF
+}
+
+parse_opt() {
+    case "$1" in
+        --)
+            no_more_opts=1
+            ;;
+        --semi-tethered)
+            semi_tethered=1
+            ;;
+        --dfuhelper)
+            dfuhelper=1
+            ;;
+        --no-baseband)
+            no_baseband=1
+            ;;
+        --dfu)
+            echo "[!] DFU mode devices are now automatically detected and --dfu is deprecated"
+            ;;
+        --debug)
+            debug=1
+            ;;
+        --help)
+            print_help
+            exit 0
+            ;;
+        *)
+            echo "[-] Unknown option $1. Use $0 --help for help."
+            exit 1;
+    esac
+}
+
+parse_arg() {
+    arg_count=$((arg_count + 1))
+    case "$1" in
+        dfuhelper)
+            dfuhelper=1
+            ;;
+        *)
+            version="$1"
+            ;;
+    esac
+}
+
+parse_cmdline() {
+    for arg in $@; do
+        if [[ "$arg" == --* ]] && [ -z "$no_more_opts" ]; then
+            parse_opt "$arg";
+        elif [ "$arg_count" -lt "$max_args" ]; then
+            parse_arg "$arg";
+        else
+            echo "[-] Too many arguments. Use $0 --help for help.";
+            exit 1;
+        fi
+    done
+}
+
+recovery_fix_auto_boot() {
+    "$dir"/irecovery -c "setenv auto-boot false"
+    "$dir"/irecovery -c "saveenv"
+
+    if [ "$semi_tethered" = "1" ]; then
+        "$dir"/irecovery -c "setenv auto-boot true"
+        "$dir"/irecovery -c "saveenv"
+    fi
+}
+
+_info() {
+    if [ "$1" = 'recovery' ]; then
+        echo $("$dir"/irecovery -q | grep "$2" | sed "s/$2: //")
+    elif [ "$1" = 'normal' ]; then
+        echo $("$dir"/ideviceinfo | grep "$2: " | sed "s/$2: //")
+    fi
+}
+
+_pwn() {
+    pwnd=$(_info recovery PWND)
+    if [ "$pwnd" = "" ]; then
+        echo "[*] Pwning device"
+        "$dir"/gaster pwn
+        sleep 2
+        #"$dir"/gaster reset
+        #sleep 1
+    fi
+}
+
+_reset() {
+    echo "[*] Resetting DFU state"
+    "$dir"/gaster reset
+}
+
+get_device_mode() {
+    if [ "$os" = "Darwin" ]; then
+        apples="$(system_profiler SPUSBDataType | grep -B1 'Vendor ID: 0x05ac' | grep 'Product ID:' | cut -dx -f2 | cut -d' ' -f1 | tail -r 2> /dev/null)"
+    elif [ "$os" = "Linux" ]; then
+        apples="$(lsusb | cut -d' ' -f6 | grep '05ac:' | cut -d: -f2)"
+    fi
+    local device_count=0
+    local usbserials=""
+    for apple in $apples; do
+        case "$apple" in
+            12ab)
+            device_mode=normal
+            device_count=$((device_count+1))
+            ;;
+            12a8)
+            device_mode=normal
+            device_count=$((device_count+1))
+            ;;
+            1281)
+            device_mode=recovery
+            device_count=$((device_count+1))
+            ;;
+            1227)
+            device_mode=dfu
+            device_count=$((device_count+1))
+            ;;
+            1222)
+            device_mode=diag
+            device_count=$((device_count+1))
+            ;;
+            1338)
+            device_mode=checkra1n_stage2
+            device_count=$((device_count+1))
+            ;;
+            4141)
+            device_mode=pongo
+            device_count=$((device_count+1))
+            ;;
+        esac
+    done
+    if [ "$device_count" = "0" ]; then
+        device_mode=none
+    elif [ "$device_count" -ge "2" ]; then
+        echo "[-] Please attach only one device" > /dev/tty
+        kill -30 0
+        exit 1;
+    fi
+    if [ "$os" = "Linux" ]; then
+        usbserials=$(cat /sys/bus/usb/devices/*/serial)
+    elif [ "$os" = "Darwin" ]; then
+        usbserials=$(system_profiler SPUSBDataType | grep 'Serial Number' | cut -d: -f2- | sed 's/ //' 2> /dev/null)
+    fi
+    if grep -qE '(ramdisk tool|SSHRD_Script) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{1,2} [0-9]{1,4} [0-9]{2}:[0-9]{2}:[0-9]{2}' <<< "$usbserials"; then
+        device_mode=ramdisk
+    fi
+    echo "$device_mode"
+}
+
+_wait() {
+    if [ "$(get_device_mode)" != "$1" ]; then
+        echo "[*] Waiting for device in $1 mode"
+    fi
+
+    while [ "$(get_device_mode)" != "$1" ]; do
+        sleep 1
+    done
+
+    if [ "$1" = 'recovery' ]; then
+        recovery_fix_auto_boot;
+    fi
+}
+
+_dfuhelper() {
+    local step_one;
+    if [[ "$1" = 0x801* && "$deviceid" != *"iPad"* ]]; then
+        step_one="Hold volume down + side button"
+    else
+        step_one="Hold home + power button"
+    fi
+    echo "[*] Press any key when ready for DFU mode"
+    read -n 1 -s
+    step 3 "Get ready"
+    step 4 "$step_one" &
+    sleep 3
+    "$dir"/irecovery -c "reset"
+    step 1 "Keep holding"
+    if [[ "$1" = "0x801"* && "$deviceid" != *"iPad"* ]]; then
+        step 10 'Release side button, but keep holding volume down'
+    else
+        step 10 'Release power button, but keep holding home button'
+    fi
+    sleep 1
+    
+    if [ "$(get_device_mode)" = "dfu" ]; then
+        echo "[*] Device entered DFU!"
+    else
+        echo "[-] Device did not enter DFU mode, rerun the script and try again"
+    fi
+}
+
+_kill_if_running() {
+    if (pgrep -u root -xf "$1" &> /dev/null > /dev/null); then
+        # yes, it's running as root. kill it
+        sudo killall $1
+    else
+        if (pgrep -x "$1" &> /dev/null > /dev/null); then
+            killall $1
+        fi
+    fi
+}
+
+_exit_handler() {
+    [ $? -eq 0 ] && exit
+    echo "[-] An error occurred"
+
+    cd logs
+    mv "$log".log FAIL_${log}.log
+    cd ..
+
+    echo "[*] A failure log has been made. If you're going ask for help, please attach the latest log."
+}
+trap _exit_handler EXIT
+
+# ===========
+# Fixes
+# ===========
+
+# ============
+# Dependencies
+# ============
+
+# Download gaster
+if [ -e "$dir"/gaster ]; then
+    "$dir"/gaster &> /dev/null > /dev/null | grep -q 'usb_timeout: 5' && rm "$dir"/gaster
+fi
+
+if [ ! -e "$dir"/gaster ]; then
+    curl -sLO https://nightly.link/palera1n/gaster/workflows/makefile/main/gaster-"$os".zip
+    unzip gaster-"$os".zip
+    mv gaster "$dir"/
+    rm -rf gaster gaster-"$os".zip
+fi
+
+# Check for pyimg4
+if ! python3 -c 'import pkgutil; exit(not pkgutil.find_loader("pyimg4"))'; then
+    echo '[-] pyimg4 not installed. Press any key to install it, or press ctrl + c to cancel'
+    read -n 1 -s
+    python3 -m pip install pyimg4
+fi
+
+# ============
+# Prep
+# ============
+
+# Update submodules
+git submodule update --init --recursive
+
+# Re-create work dir if it exists, else, make it
+if [ -e work ]; then
+    rm -rf work
+    mkdir work
+else
+    mkdir work
+fi
+
+chmod +x "$dir"/*
+cp ramdisk_files/* ramdisk
+chmod +x ramdisk/sshrd.sh
+#if [ "$os" = 'Darwin' ]; then
+#    xattr -d com.apple.quarantine "$dir"/*
+#fi
+
+# ============
+# Start
+# ============
+
+echo "dirs_cleaner fixer | Version $version-$branch-$commit"
+echo "Written by Ploosh | based off of palera1n"
+echo ""
+
+version=""
+parse_cmdline "$@"
+
+if [ "$debug" = "1" ]; then
+    set -o xtrace
+fi
+
+# Get device's iOS version from ideviceinfo if in normal mode
+echo "[*] Waiting for devices"
+while [ "$(get_device_mode)" = "none" ]; do
+    sleep 1;
+done
+echo $(echo "[*] Detected $(get_device_mode) mode device" | sed 's/dfu/DFU/')
+
+if grep -E 'pongo|checkra1n_stage2|diag' <<< "$(get_device_mode)"; then
+    echo "[-] Detected device in unsupported mode '$(get_device_mode)'"
+    exit 1;
+fi
+
+if [ "$(get_device_mode)" != "normal" ] && [ -z "$version" ] && [ "$dfuhelper" != "1" ]; then
+    echo "[-] You must pass the version your device is on when not starting from normal mode"
+    exit
+fi
+
+if [ "$(get_device_mode)" = "ramdisk" ]; then
+    # If a device is in ramdisk mode, perhaps iproxy is still running?
+    _kill_if_running iproxy
+    echo "[*] Rebooting device in SSH Ramdisk"
+    if [ "$os" = 'Linux' ]; then
+        sudo "$dir"/iproxy 2222 22 &
+    else
+        "$dir"/iproxy 2222 22 &
+    fi
+    sleep 1
+    remote_cmd "/usr/sbin/nvram auto-boot=false"
+    remote_cmd "/sbin/reboot"
+    _kill_if_running iproxy
+    _wait recovery
+fi
+
+if [ "$(get_device_mode)" = "normal" ]; then
+    version=$(_info normal ProductVersion)
+    arch=$(_info normal CPUArchitecture)
+    if [ "$arch" = "arm64e" ]; then
+        echo "[-] this script doesn't, and never will, work on non-checkm8 devices"
+        exit
+    fi
+    echo "Hello, $(_info normal ProductType) on $version!"
+
+    echo "[*] Switching device into recovery mode..."
+    "$dir"/ideviceenterrecovery $(_info normal UniqueDeviceID)
+    _wait recovery
+fi
+
+# Grab more info
+echo "[*] Getting device info..."
+cpid=$(_info recovery CPID)
+model=$(_info recovery MODEL)
+deviceid=$(_info recovery PRODUCT)
+
+if [ "$dfuhelper" = "1" ]; then
+    echo "[*] Running DFU helper"
+    _dfuhelper "$cpid"
+    exit
+fi
+
+if [ ! "$ipsw" = "" ]; then
+    ipswurl=$ipsw
+else
+    if [[ "$deviceid" == *"iPad"* ]]; then
+        device_os=iPadOS
+        device=iPad
+    elif [[ "$deviceid" == *"iPod"* ]]; then
+        device_os=iOS
+        device=iPod
+    else
+        device_os=iOS
+        device=iPhone
+    fi
+
+    buildid=$(curl -sL https://api.ipsw.me/v4/ipsw/$version | "$dir"/jq '[.[] | select(.identifier | startswith("'$device'")) | .buildid][0]' --raw-output)
+    if [ "$buildid" == "19B75" ]; then
+        buildid=19B74
+    fi
+    ipswurl=$(curl -sL https://api.appledb.dev/ios/$device_os\;$buildid.json | "$dir"/jq -r .devices\[\"$deviceid\"\].ipsw)
+fi
+
+# Have the user put the device into DFU
+if [ "$(get_device_mode)" != "dfu" ]; then
+    recovery_fix_auto_boot;
+    _dfuhelper "$cpid"
+fi
+sleep 2
+
+# ============
+# Ramdisk
+# ============
+
+# Dump blobs, and install pogo if needed
+if [ ! -f blobs/"$deviceid"-"$version".der ]; then
+    cd ramdisk
+    chmod +x sshrd.sh
+    echo "[*] Creating ramdisk"
+    ./sshrd.sh `if [[ "$version" == *"16"* ]]; then echo "16.0.3"; else echo "15.6"; fi`
+
+    echo "[*] Booting ramdisk"
+    ./sshrd.sh boot
+    cd ..
+
+    # if known hosts file exists, remove it
+    if [ -f ~/.ssh/known_hosts ]; then
+        rm ~/.ssh/known_hosts
+    fi
+
+    # Execute the commands once the rd is booted
+    _kill_if_running iproxy
+    if [ "$os" = 'Linux' ]; then
+        sudo "$dir"/iproxy 2222 22 &
+    else
+        "$dir"/iproxy 2222 22 &
+    fi
+
+    while ! (remote_cmd "echo connected" &> /dev/null); do
+        sleep 1
+    done
+
+    remote_cmd "/usr/bin/mount_filesystems"
+
+    echo "[*] Testing for baseband presence"
+    if [ "$(remote_cmd "/usr/bin/mgask HasBaseband | grep -E 'true|false'")" = "true" ] && [ "${cpid}" == *"0x700"* ]; then
+            disk=7
+    elif [ "$(remote_cmd "/usr/bin/mgask HasBaseband | grep -E 'true|false'")" = "false" ]; then
+        if [ "${cpid}" == *"0x700"* ]; then
+            disk=6
+        else
+            disk=7
+        fi
+    fi
+
+    if [ -z "$semi_tethered" ]; then
+        disk=1
+    fi
+
+    if [[ "$version" == *"16"* ]]; then
+        fs=disk1s$disk
+    else
+        fs=disk0s1s$disk
+    fi
+
+    has_active=$(remote_cmd "ls /mnt6/active" 2> /dev/null)
+    if [ ! "$has_active" = "/mnt6/active" ]; then
+        echo "[!] Active file does not exist! Please use SSH to create it"
+        echo "    /mnt6/active should contain the name of the UUID in /mnt6"
+        echo "    When done, type reboot in the SSH session, then rerun the script"
+        echo "    ssh root@localhost -p 2222"
+        exit
+    fi
+    active=$(remote_cmd "cat /mnt6/active" 2> /dev/null)
+
+    if [ "$semi_tethered" = "1" ]; then
+        remote_cmd "/sbin/mount_apfs /dev/$fs /mnt8"
+        remote_cmd "cp /mnt1/usr/libexec/dirs_cleaner /mnt1/usr/libexec/dirs_cleaner"
+    else
+        snapshot="com.apple.os.update-$active"
+        while [ "$(remote_cmd \"[ -s /mnt4/usr/libexec/dirs_cleaner ]; echo $?\")" = "1" ]; do
+            remote_cmd "umount /mnt4"
+            remote_cmd "snaputil -s $snapshot /mnt1 /mnt4"
+        done
+        remote_cmd "cp /mnt4/usr/libexec/dirs_cleaner /mnt1/usr/libexec/dirs_cleaner"
+    fi
+
+    sleep 2
+    echo "[*] Rebooting device (you may force reboot)"
+    if [ -z "$semi_tethered" ]; then
+        echo "Your device will reboot into recovery mode, re-run palera1n and your device should boot"
+    fi
+    remote_cmd "/sbin/reboot"
+    sleep 1
+    _kill_if_running iproxy
+fi
+
+cd logs
+mv "$log".log SUCCESS_${log}.log
+cd ..
+
+rm -rf work rdwork
+echo ""
+echo "Done!"
+echo "dirs_cleaner should now be restored to normal!"
+
+} | tee logs/${log}.log
---
+++ ramdisk_files/sshrd.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+
+set -e
+
+oscheck=$(uname)
+
+ERR_HANDLER () {
+    [ $? -eq 0 ] && exit
+    echo "[-] An error occurred"
+    rm -rf work
+}
+
+trap ERR_HANDLER EXIT
+
+# Check for pyimg4
+if ! python3 -c 'import pkgutil; exit(not pkgutil.find_loader("pyimg4"))'; then
+    echo '[-] pyimg4 not installed. Press any key to install it, or press ctrl + c to cancel'
+    read -n 1 -s
+    python3 -m pip install pyimg4
+fi
+
+# git submodule update --init --recursive
+
+if [ ! -e "$oscheck"/gaster ]; then
+    curl -sLO https://nightly.link/palera1n/gaster/workflows/makefile/main/gaster-"$oscheck".zip
+    unzip gaster-"$oscheck".zip
+    mv gaster "$oscheck"/
+    rm -rf gaster gaster-"$oscheck".zip
+fi
+
+chmod +x "$oscheck"/*
+
+if [ "$oscheck" = 'Darwin' ]; then
+    if ! (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (DFU Mode)' >> /dev/null); then
+        echo "[*] Waiting for device in DFU mode"
+    fi
+    
+    while ! (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (DFU Mode)' >> /dev/null); do
+        sleep 1
+    done
+else
+    if ! (lsusb 2> /dev/null | grep ' Apple, Inc. Mobile Device (DFU Mode)' >> /dev/null); then
+        echo "[*] Waiting for device in DFU mode"
+    fi
+    
+    while ! (lsusb 2> /dev/null | grep ' Apple, Inc. Mobile Device (DFU Mode)' >> /dev/null); do
+        sleep 1
+    done
+fi
+
+check=$("$oscheck"/irecovery -q | grep CPID | sed 's/CPID: //')
+replace=$("$oscheck"/irecovery -q | grep MODEL | sed 's/MODEL: //')
+deviceid=$("$oscheck"/irecovery -q | grep PRODUCT | sed 's/PRODUCT: //')
+ipswurl=$(curl -sL "https://api.ipsw.me/v4/device/$deviceid?type=ipsw" | "$oscheck"/jq '.firmwares | .[] | select(.version=="'$1'")' | "$oscheck"/jq -s '.[0] | .url' --raw-output)
+if [[ "$deviceid" == *"iPad"* ]]; then
+    if [[ "$1" == *"16"* ]]; then
+        ipswurl=$(curl -sL https://api.appledb.dev/ios/iPadOS\;20A5349b.json | "$oscheck"/jq -r .devices\[\"$deviceid\"\].ipsw)
+    fi
+fi
+
+if [ -e work ]; then
+    rm -rf work
+fi
+
+if [ ! -e sshramdisk ]; then
+    mkdir sshramdisk
+fi
+
+if [ "$1" = 'boot' ]; then
+    if [ ! -e sshramdisk/iBSS.img4 ]; then
+        echo "[-] Please create an SSH ramdisk first!"
+        exit
+    fi
+
+    "$oscheck"/gaster pwn
+    sleep 1
+    "$oscheck"/gaster reset
+    sleep 1
+    "$oscheck"/irecovery -f sshramdisk/iBSS.img4
+    sleep 2
+    "$oscheck"/irecovery -f sshramdisk/iBEC.img4
+    if [[ "$check" = *"0x801"* ]]; then
+        sleep 1
+        "$oscheck"/irecovery -c go
+    fi
+    sleep 1
+    "$oscheck"/irecovery -f sshramdisk/bootlogo.img4
+    sleep 1
+    "$oscheck"/irecovery -c "setpicture 0x1"
+    sleep 1
+    "$oscheck"/irecovery -f sshramdisk/ramdisk.img4
+    sleep 1
+    "$oscheck"/irecovery -c ramdisk
+    sleep 1
+    "$oscheck"/irecovery -f sshramdisk/devicetree.img4
+    sleep 1
+    "$oscheck"/irecovery -c devicetree
+    sleep 1
+    "$oscheck"/irecovery -f sshramdisk/trustcache.img4
+    sleep 1
+    "$oscheck"/irecovery -c firmware
+    sleep 1
+    "$oscheck"/irecovery -f sshramdisk/kernelcache.img4
+    sleep 1
+    "$oscheck"/irecovery -c bootx
+
+    exit
+fi
+
+if [ -z "$1" ]; then
+    printf "1st argument: iOS version for the ramdisk\n"
+    exit
+fi
+
+if [ ! -e work ]; then
+    mkdir work
+fi
+
+"$oscheck"/gaster pwn
+"$oscheck"/img4tool -e -s shsh/"${check}".shsh -m work/IM4M
+
+cd work
+../"$oscheck"/pzb -g BuildManifest.plist "$ipswurl"
+../"$oscheck"/pzb -g "$(awk "/""${replace}""/{x=1}x&&/iBSS[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1)" "$ipswurl"
+../"$oscheck"/pzb -g "$(awk "/""${replace}""/{x=1}x&&/iBEC[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1)" "$ipswurl"
+../"$oscheck"/pzb -g "$(awk "/""${replace}""/{x=1}x&&/DeviceTree[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1)" "$ipswurl"
+
+if [ "$oscheck" = 'Darwin' ]; then
+    ../"$oscheck"/pzb -g Firmware/"$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)".trustcache "$ipswurl"
+else
+    ../"$oscheck"/pzb -g Firmware/"$(../Linux/PlistBuddy BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g')".trustcache "$ipswurl"
+fi
+
+../"$oscheck"/pzb -g "$(awk "/""${replace}""/{x=1}x&&/kernelcache.release/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1)" "$ipswurl"
+
+if [ "$oscheck" = 'Darwin' ]; then
+    ../"$oscheck"/pzb -g "$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" "$ipswurl"
+else
+    ../"$oscheck"/pzb -g "$(../Linux/PlistBuddy BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g')" "$ipswurl"
+fi
+
+cd ..
+"$oscheck"/gaster decrypt work/"$(awk "/""${replace}""/{x=1}x&&/iBSS[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//')" work/iBSS.dec
+"$oscheck"/gaster decrypt work/"$(awk "/""${replace}""/{x=1}x&&/iBEC[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//')" work/iBEC.dec
+"$oscheck"/iBoot64Patcher work/iBSS.dec work/iBSS.patched
+"$oscheck"/img4 -i work/iBSS.patched -o sshramdisk/iBSS.img4 -M work/IM4M -A -T ibss
+"$oscheck"/iBoot64Patcher work/iBEC.dec work/iBEC.patched -b "rd=md0 debug=0x2014e wdt=-1 serial=3 `if [ "$check" = '0x8960' ] || [ "$check" = '0x7000' ] || [ "$check" = '0x7001' ]; then echo "-restore"; fi`" -n
+"$oscheck"/img4 -i work/iBEC.patched -o sshramdisk/iBEC.img4 -M work/IM4M -A -T ibec
+
+"$oscheck"/img4 -i work/"$(awk "/""${replace}""/{x=1}x&&/kernelcache.release/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1)" -o work/kcache.raw
+"$oscheck"/Kernel64Patcher work/kcache.raw work/kcache.patched -a
+python3 kerneldiff.py work/kcache.raw work/kcache.patched work/kc.bpatch
+"$oscheck"/img4 -i work/"$(awk "/""${replace}""/{x=1}x&&/kernelcache.release/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1)" -o sshramdisk/kernelcache.img4 -M work/IM4M -T rkrn -P work/kc.bpatch `if [ "$oscheck" = 'Linux' ]; then echo "-J"; fi`
+"$oscheck"/img4 -i work/"$(awk "/""${replace}""/{x=1}x&&/DeviceTree[.]/{print;exit}" work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | sed 's/Firmware[/]all_flash[/]//')" -o sshramdisk/devicetree.img4 -M work/IM4M -T rdtr
+
+if [ "$oscheck" = 'Darwin' ]; then
+    "$oscheck"/img4 -i work/"$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)".trustcache -o sshramdisk/trustcache.img4 -M work/IM4M -T rtsc
+    "$oscheck"/img4 -i work/"$(/usr/bin/plutil -extract "BuildIdentities".0."Manifest"."RestoreRamDisk"."Info"."Path" xml1 -o - work/BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1 | head -1)" -o work/ramdisk.dmg
+else
+    "$oscheck"/img4 -i work/"$(Linux/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g')".trustcache -o sshramdisk/trustcache.img4 -M work/IM4M -T rtsc
+    "$oscheck"/img4 -i work/"$(Linux/PlistBuddy work/BuildManifest.plist -c "Print BuildIdentities:0:Manifest:RestoreRamDisk:Info:Path" | sed 's/"//g')" -o work/ramdisk.dmg
+fi
+
+if [ "$oscheck" = 'Darwin' ]; then
+    hdiutil resize -size 256MB work/ramdisk.dmg
+    hdiutil attach -mountpoint /tmp/SSHRD work/ramdisk.dmg
+
+    "$oscheck"/gtar -x --no-overwrite-dir -f other/ramdisk.tar.gz -C /tmp/SSHRD/
+
+    hdiutil detach -force /tmp/SSHRD
+    hdiutil resize -sectors min work/ramdisk.dmg
+else
+    if [ -f other/ramdisk.tar.gz ]; then
+        gzip -d other/ramdisk.tar.gz
+    fi
+
+    "$oscheck"/hfsplus work/ramdisk.dmg grow 300000000 > /dev/null
+    "$oscheck"/hfsplus work/ramdisk.dmg untar other/ramdisk.tar > /dev/null
+fi
+python3 -m pyimg4 im4p create -i work/ramdisk.dmg -o work/ramdisk.im4p -f rdsk
+python3 -m pyimg4 img4 create -p work/ramdisk.im4p -m work/IM4M -o sshramdisk/ramdisk.img4
+"$oscheck"/img4 -i other/bootlogo.im4p -o sshramdisk/bootlogo.img4 -M work/IM4M -A -T rlgo
+
+rm -rf work
